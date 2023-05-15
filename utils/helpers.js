958c654b6f0dbbc19a4f16046e4c5c4b804ec0e2
Erin Keller <aestheticartist@gmail.com>
2023-05-15 15:29:42 -0400

    Added comments to utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,29 @@
+// initialize variables
 const moment = require('moment');
 
 module.exports = {
+    /**
+     * @isActive
+     * checks if link in header matches
+     * the current path, if so, it adds
+     * an active class to the nav item
+     */
     isActive: function(url) {
         return (url === this.url) ? 'active' : '';
     },
-    json: function(context) {
-        return JSON.stringify(context);
-    },
+    /**
+     * @formatDate
+     * accepts a date object and uses
+     * moment.js to format the date 
+     * and time
+     */
     formatDate: function(date) {
         return moment(date).format('MMMM Do YYYY, h:mm a');
     },
+    /**
+     * @toJson
+     * stringifies a handlebar object
+     */
     toJson: function (obj) {
         return JSON.stringify(obj, null, 3);
     }
