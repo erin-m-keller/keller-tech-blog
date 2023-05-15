c49e2242bba28e9a55782c3fbcf3a7482b421672
Erin Keller <aestheticartist@gmail.com>
2023-05-15 15:35:01 -0400

    Added comments to the server file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// initialize variables
 const path = require('path'),
       express = require('express'),
       session = require('express-session'),
@@ -10,28 +11,33 @@ const path = require('path'),
       PORT = process.env.PORT || 3001,
       hbs = exphbs.create({ helpers }),
       sess = {
-        secret: process.env.SECRET,
+        secret: process.env.SECRET, // secret used to sign the session ID cookie
         cookie: {
-          maxAge: 1800000, // Session expiration time in milliseconds (30 minutes)
+          maxAge: 1800000, // session expiration in milliseconds (30 minutes)
         },
-        resave: false,
-        saveUninitialized: true,
+        resave: false, // forces the session to be saved back to the session store, even if it hasn't been modified
+        saveUninitialized: true, // forces a session that is "uninitialized" to be saved to the store
         store: new SequelizeStore({
-          db: sequelize
+          db: sequelize // sequelize database connection
         })
       };
-
+      
+// Use the session middleware
 app.use(session(sess));
+// configure the view engine to use Handlebars templates
+app.engine('handlebars', hbs.engine); 
+// set the default view engine to handlebars
+app.set('view engine', 'handlebars'); 
+// parse incoming requests with JSON payloads
+app.use(express.json()); 
+// parse incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true })); 
+// serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public'))); 
+// mount the routes from the controllers file
+app.use(routes); 
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
+// sync sequelize models and start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
