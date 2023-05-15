4dc693fbf28fdba668ead7fe1610d70e6331bbaf
Erin Keller <aestheticartist@gmail.com>
2023-05-15 16:19:04 -0400

    Added comments to logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,23 @@
+/**
+ * @logout
+ * Function handles logging the user out
+ */
 const logout = async () => {
-    const response = await fetch('/api/auth/logout', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+  // the response received from the GET request
+  const response = await fetch('/api/auth/logout', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  // if reponse ok
+  if (response.ok) {
+    // redirect to the home page
+    document.location.replace('/');
+  } 
+  // else if response is not ok
+  else {
+    // alert prompt with response status text
+    alert(response.statusText);
+  }
+};
+// event listeners - on click
+document.querySelector('#logout').addEventListener('click', logout);
